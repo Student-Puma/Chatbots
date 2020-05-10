385009712a1a8538832c460519c4718a1dd7fd73
Pumita <kikefontanlorenzo@gmail.com>
2020-05-10 06:04:09 +0200

    :tada: Empezand el Proyecto 2

---
+++ tester/run.sh
@@ -0,0 +1,2 @@
+java -cp lib/Ab.jar Main bot=../../app/src/resources/bots/proyecto action=aiml2csv trace=false
+java -cp lib/Ab.jar Main bot=../../app/src/resources/bots/proyecto action=chat trace=false
